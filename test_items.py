4d5be9734ff3b0f6e363d1a057d92c91c0b5ea32
quiprodest <52920692+quiprodest@users.noreply.github.com>
2019-07-29 13:30:17 +0300

    Add template for adding 'time.sleep(30)'

--- test_items.py
+++ test_items.py
@@ -1,5 +1,7 @@
+import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket_is_here(browser):
     browser.get(link)
+    # time.sleep(30)
     browser.find_element_by_css_selector(".btn-add-to-basket")
